8489923d71b520500bc867a57986f2170044dd4e
CactusFantastico <42050275+CactusFantastico@users.noreply.github.com>
2018-08-23 23:38:08 -0700

    latest version

--- DnD5e_Character_Generator.py
+++ DnD5e_Character_Generator.py
@@ -17,12 +17,14 @@ def dice_size_picker(dx):
         return 12
     if dx == 'd20':
         return 20
+    else:
+        return dx.__str__()
 
 
-def dice(dnumber =1, dsize =6):
+def dice(dnumber=1, dsize=6):
     dice_size_picker(dx=dsize)
-    for i in range(0,dnumber):
-        rolls.append(random.randint(1,dsize))
+    for i in range(0, dnumber):
+        rolls.append(random.randint(1, dsize))
 
 
 number_of_dice = int(input("How many dice would you like to roll? "))
@@ -30,5 +32,5 @@ dice_picker = input("""what size dice would you like to roll?
     Choices are d2, d4, d6, d10, d12, and d20""")
 
 dice_size = dice_size_picker(dx=dice_picker)
-dice(dnumber=number_of_dice, dsize=dice_size )
+dice(dnumber=number_of_dice, dsize=dice_size)
 print(rolls)
